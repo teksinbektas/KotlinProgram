5f60f8174de420710b0ba8dc8d01b7d29f728c8f
Teksin Bektaş <116187705+teksinbektas@users.noreply.github.com>
2025-04-27 00:11:54 +0300

    Variable identification
    define variables in KOTLIN

---
+++ Odev1.kt
@@ -0,0 +1,99 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    val sehir = "istanbul"
+    var ulke = "Türkiye"
+    val telefon = 532
+    var postaKodu = 34150
+    val email ="asdasd@khbsaf"
+    var meslek = "Öğrenci"
+    var stokMiktari = 12
+    val musteriAdi = "Ali"
+    val bakiye = 12
+    var dogumGunu = 21
+    var maas = 2567
+    var medeniDurum = "Bekar"
+    var urunYorum = "yorum"
+    var odemeTarihi = 12
+    val odeme = "Ödeme yapıldı"
+    var siparisAdi = "abcma"
+    var arabaModeli = "mercedes"
+    var kitapAdi = "1984"
+    val yayinlanmaTarihi ="03/01/2025"
+    var indirimMiktari = 12
+    val odaSayisi = 2
+    val enlem = "6342"
+    val boylam = 2442
+    var urunAdi = "bla bla"
+    val yemekFiyati = 8765
+    var marka = "LCW"
+    val muzikAdi = "bla bla"
+    val videoSuresi = 13
+    var urunPuani= 3
+    var resimAdi = "cmpe12.png"
+    val dosyaFormati = "docx"
+    var renk ="mavi"
+    val renkKodu = "#2KAA981"
+    val telefonModeli = "Samsung"
+    val ekranBoyutu= 6.1
+    var agirlik = 265
+    val ulusalgun = 23
+    var tatilgunu = 12
+    var rezervasyonTarihi = "12/03/2025"
+    val sokakAdi = "maçka cad"
+    var otobusHatti  = 14
+    val kalanDakika = 41
+    var takipKodu="e21vgoe235"
+    var kuponSuresi = 12
+    var kuponKodu = "13sm1p42msd"
+    var faturaAdresi = "maçka sırılsıklam caddesi"
+
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorumu: $urunYorum")
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme")
+    println("Sipariş Adı: $siparisAdi")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlanma Tarihi: $yayinlanmaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+    println("Ulusal Günü: $ulusalgun")
+    println("Tatil Günü: $tatilgunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hatti: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+     
+}
